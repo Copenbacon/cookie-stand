cd1d02b5d2ea6d61f89d1e04b46f8e4f6cc84838
Copenbacon <sclary50@gmail.com>
2016-09-26 12:26:53 -0700

    structure set up

---
+++ app.js
@@ -0,0 +1,3 @@
+//object literal for each location (5)
+//Properties: mincusto, maxcusto, avgcookpercusto, locationName, Hours[6am, ... 8pm] (store outside object), randCustPerHour[]
+//Methods: randCustPerHourGen, calculateAndStoreCookiesPurchasedEachHour
