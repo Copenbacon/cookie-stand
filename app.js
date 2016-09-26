90f92f480a33161fadbf8fdc5c89d4fdf57aceaa
Copenbacon <sclary50@gmail.com>
2016-09-26 12:58:10 -0700

    random customer generator works for First and Pike, set up script link to sales html page, defined ul's and id's on sales html page

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+'use strict';
 //object literal for each location (5)
-//Properties: mincusto, maxcusto, avgcookpercusto, locationName, Hours[6am, ... 8pm] (store outside object), randCustPerHour[]
-//Methods: randCustPerHourGen, calculateAndStoreCookiesPurchasedEachHour
+//Properties: mincusto, maxcusto, avgcookpercusto, locationName, randCustPerHour[],  results[], totalCookiesSoldPerHour, totalDailySales
+//Methods: randCustPerHourGen, calculateAndStoreCookiesPurchasedEachHour, resultsGenerator(avgcookpercusto*randCustPerHour stored with hour[i]), totalDailySalesMethod(calculateAndStoreTotalCookiesPurchased), render(display list of store data (appendChild))
+
+//outsideVariables
+//Hours[6am, ... 8pm] (store outside object),
+
+//<ul id = locationName></ul> document.getElementById()
+
+var storeHours = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM',];
+
+var firstAndPike = {
+  //Properties
+  locationName : '1st and Pike',
+  minCustPerHour : 23,
+  maxCustPerHour : 65,
+  avgCookiesPerSale : 6.3,
+  randCustPerHour : [],
+  results : [],
+  totalCookiesSoldPerHour : null ,
+  totalDailySales : null,
+
+  //Methods
+  randCustPerHourGen : function(minCustPerHour, maxCustPerHour) {
+    this.minCustPerHour = Math.ceil(this.minCustPerHour);
+    this.maxCustPerHour = Math.floor(this.maxCustPerHour);
+    return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+  }
+  //
+  // calculateAndStoreCookiesPurchasedEachHour : function() {
+  //
+  // }
+  //
+  // resultsGenerator : function() {
+  //   // avgCookiesPerSale*randCustPerHour
+  //   //stored with storeHours[i];
+  // }
+  //
+  // totalDailySalesMethod : function() {
+  //
+  // }
+  //
+  // render : function() {
+  //
+  // }
+};
